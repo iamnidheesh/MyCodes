6eb8f8840992d52190c5746dd7d26a72c073366e
Nidheesh <nidheeshpandey@gmail.com>
2017-08-29 14:48:12 +0530

    pacman using dfs

---
+++ AI_LAB/pacman/ass3/pacmandfs.py
@@ -0,0 +1,58 @@
+
+#nodes explored
+
+def check(travel):
+	#print(travel[0],travel[1],vis[travel[0]][travel[1]])
+	if(vis[travel[0]][travel[1]] == -1 and travel[0] >= 0 and travel[0] < dim[0] and travel[1] >= 0 and travel[1] < dim[1]):
+		#print(travel[0],travel[1])
+		if(grid[travel[0]][travel[1]] == '-' or grid[travel[0]][travel[1]] == '.'):
+			#print(True)
+			return True
+		else:
+			#print(False)
+			return False
+	else:
+		return False	
+	
+def dfs(s,d) :
+	l = []
+	l.append(s)
+	vis[s[0]][s[1]] = 0
+	result = [-1,-1]
+	while(len(l) != 0):
+		curr = l.pop()
+		print(curr)
+		if(curr[0] == d[0] and curr[1] == d[1]) :
+			result = vis[curr[0]][curr[1]]
+			break
+		if(check([curr[0]-1,curr[1]])):
+			#print("1")
+			vis[curr[0]-1][curr[1]] = 1 + vis[curr[0]][curr[1]] 
+			l.append([curr[0]-1,curr[1]])
+		if(check([curr[0],curr[1]-1])):
+			#print("2")
+			vis[curr[0]][curr[1]-1] = 1 + vis[curr[0]][curr[1]] 
+			l.append([curr[0],curr[1]-1])
+		if(check([curr[0],curr[1]+1])):
+			#print("3")
+			vis[curr[0]][curr[1]+1] = 1 + vis[curr[0]][curr[1]] 
+			l.append([curr[0],curr[1]+1])
+		if(check([curr[0]+1,curr[1]])):
+			#print("4")
+			vis[curr[0]+1][curr[1]] = 1 + vis[curr[0]][curr[1]] 
+			l.append([curr[0]+1,curr[1]])
+			
+		#print(l)
+		
+	return result
+
+
+
+source = list(map(int,input().split()))
+destination = list(map(int,input().split()))
+dim = list(map(int,input().split()))
+grid = [input() for i in range(dim[0])]
+#print(grid)
+vis = [[-1]*dim[1] for i in range(dim[0])]
+result = dfs(source,destination)
+print("No. of steps using dfs :" ,result)
