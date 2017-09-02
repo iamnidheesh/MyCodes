c3581bb17d75cd1aa686e44ab997eaa87e44a2e0
Nidheesh <nidheeshpandey@gmail.com>
2017-09-02 13:59:09 +0530

    output with path and smaller code

--- AI_LAB/pacman/ass3/pacmanbfs.py
+++ AI_LAB/pacman/ass3/pacmanbfs.py
@@ -1,50 +1,43 @@
 
 #nodes explored
+#path
 from collections import deque
 def check(travel):
-	#print(travel[0],travel[1],vis[travel[0]][travel[1]])
 	if(vis[travel[0]][travel[1]] == -1 and travel[0] >= 0 and travel[0] < dim[0] and travel[1] >= 0 and travel[1] < dim[1]):
-		#print(travel[0],travel[1])
 		if(grid[travel[0]][travel[1]] == '-' or grid[travel[0]][travel[1]] == '.'):
-			#print(True)
 			return True
 		else:
-			#print(False)
 			return False
 	else:
 		return False	
-def bfs(s,d) :
+	
+def dfs(s,d) :
 	l = deque()
 	l.append(s)
-	vis[s[0]][s[1]] = 1
-	result = [-1,-1]
+	vis[s[0]][s[1]] = 0
+	result = -1
+	count = 0
+	parent = [[(-1,-1)]*dim[1] for i in range(dim[0])]
+	row = [-1,0,0,1]
+	col = [0,-1,1,0]
 	while(len(l) != 0):
 		curr = l.popleft()
-		print(curr)
+		count += 1
+		explored.append(curr)
 		if(curr[0] == d[0] and curr[1] == d[1]) :
 			result = vis[curr[0]][curr[1]]
 			break
-		if(check([curr[0]-1,curr[1]])):
-			#print("1")
-			vis[curr[0]-1][curr[1]] = 1 + vis[curr[0]][curr[1]] 
-			l.append([curr[0]-1,curr[1]])
-		if(check([curr[0],curr[1]-1])):
-			#print("2")
-			vis[curr[0]][curr[1]-1] = 1 + vis[curr[0]][curr[1]] 
-			l.append([curr[0],curr[1]-1])
-		if(check([curr[0],curr[1]+1])):
-			#print("3")
-			vis[curr[0]][curr[1]+1] = 1 + vis[curr[0]][curr[1]] 
-			l.append([curr[0],curr[1]+1])
-		if(check([curr[0]+1,curr[1]])):
-			#print("4")
-			vis[curr[0]+1][curr[1]] = 1 + vis[curr[0]][curr[1]] 
-			l.append([curr[0]+1,curr[1]])
-			
-		#print(l)
+		for index in range(4) :
+			u,v = curr[0]+row[index] , curr[1]+col[index]
+
+			if(check([u,v])):
+				vis[u][v] = 1 + vis[curr[0]][curr[1]] 
+				l.append([u,v])
+				parent[u][v] = (curr[0],curr[1])
 		
-	return result
-	
+	return (result,count,parent)
+
+
 
 source = list(map(int,input().split()))
 destination = list(map(int,input().split()))
@@ -52,5 +45,18 @@ dim = list(map(int,input().split()))
 grid = [input() for i in range(dim[0])]
 #print(grid)
 vis = [[-1]*dim[1] for i in range(dim[0])]
-result = bfs(source,destination)
-print("No. of steps using bfs :" ,result)
+explored = []
+(result,count,parent) = dfs(source,destination)
+temp = destination
+path = []
+while(parent[temp[0]][temp[1]] != (-1,-1)) :
+	path.append((temp[0],temp[1]))
+	temp = parent[temp[0]][temp[1]]
+path.append(source)
+path.reverse()
+print(count)
+for i in explored :
+	print(i[0],i[1])
+print(result)
+for i in path:
+	print(i[0],i[1])
--- AI_LAB/pacman/ass3/pacmandfs.py
+++ AI_LAB/pacman/ass3/pacmandfs.py
@@ -1,15 +1,11 @@
 
 #nodes explored
-
+#path
 def check(travel):
-	#print(travel[0],travel[1],vis[travel[0]][travel[1]])
 	if(vis[travel[0]][travel[1]] == -1 and travel[0] >= 0 and travel[0] < dim[0] and travel[1] >= 0 and travel[1] < dim[1]):
-		#print(travel[0],travel[1])
 		if(grid[travel[0]][travel[1]] == '-' or grid[travel[0]][travel[1]] == '.'):
-			#print(True)
 			return True
 		else:
-			#print(False)
 			return False
 	else:
 		return False	
@@ -18,33 +14,27 @@ def dfs(s,d) :
 	l = []
 	l.append(s)
 	vis[s[0]][s[1]] = 0
-	result = [-1,-1]
+	result = -1
+	count = 0
+	parent = [[(-1,-1)]*dim[1] for i in range(dim[0])]
+	row = [-1,0,0,1]
+	col = [0,-1,1,0]
 	while(len(l) != 0):
 		curr = l.pop()
-		print(curr)
+		count += 1
+		explored.append(curr)
 		if(curr[0] == d[0] and curr[1] == d[1]) :
 			result = vis[curr[0]][curr[1]]
 			break
-		if(check([curr[0]-1,curr[1]])):
-			#print("1")
-			vis[curr[0]-1][curr[1]] = 1 + vis[curr[0]][curr[1]] 
-			l.append([curr[0]-1,curr[1]])
-		if(check([curr[0],curr[1]-1])):
-			#print("2")
-			vis[curr[0]][curr[1]-1] = 1 + vis[curr[0]][curr[1]] 
-			l.append([curr[0],curr[1]-1])
-		if(check([curr[0],curr[1]+1])):
-			#print("3")
-			vis[curr[0]][curr[1]+1] = 1 + vis[curr[0]][curr[1]] 
-			l.append([curr[0],curr[1]+1])
-		if(check([curr[0]+1,curr[1]])):
-			#print("4")
-			vis[curr[0]+1][curr[1]] = 1 + vis[curr[0]][curr[1]] 
-			l.append([curr[0]+1,curr[1]])
-			
-		#print(l)
+		for index in range(4) :
+			u,v = curr[0]+row[index] , curr[1]+col[index]
+
+			if(check([u,v])):
+				vis[u][v] = 1 + vis[curr[0]][curr[1]] 
+				l.append([u,v])
+				parent[u][v] = (curr[0],curr[1])
 		
-	return result
+	return (result,count,parent)
 
 
 
@@ -54,5 +44,18 @@ dim = list(map(int,input().split()))
 grid = [input() for i in range(dim[0])]
 #print(grid)
 vis = [[-1]*dim[1] for i in range(dim[0])]
-result = dfs(source,destination)
-print("No. of steps using dfs :" ,result)
+explored = []
+(result,count,parent) = dfs(source,destination)
+temp = destination
+path = []
+while(parent[temp[0]][temp[1]] != (-1,-1)) :
+	path.append((temp[0],temp[1]))
+	temp = parent[temp[0]][temp[1]]
+path.append(source)
+path.reverse()
+print(count)
+for i in explored :
+	print(i[0],i[1])
+print(result)
+for i in path:
+	print(i[0],i[1])
