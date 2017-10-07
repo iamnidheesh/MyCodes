de2ef33b49b2bb700b0590f7023bcc7165f62f59
Nidheesh <nidheeshpandey@gmail.com>
2017-10-07 23:03:52 +0530

    scc application

---
+++ Hackerearth/graph/scc/AWalkToRemember.cpp
@@ -0,0 +1,59 @@
+/*
+important : Node a and b belongs to the same cycle 
+if and only if a and b are strongly connected.*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int a,b,n,ct,m;
+stack <int> st;
+vector <int> adj[100001],rev[100001];
+int vis[100001];
+map <int,int> mymap;
+void dfs(int v) {
+	vis[v] = 1;
+	for(int i = 0;i < adj[v].size();i++) {
+		int x = adj[v][i];
+		if(!vis[x]) {
+			dfs(x);
+		}
+	}
+	st.push(v);
+}
+void revdfs(int v) {
+	vis[v] = ct;
+	mymap[ct]++;
+	for(int i = 0;i < rev[v].size();i++) {
+		int x = rev[v][i];
+		if(!vis[x]) {
+			revdfs(x);
+		}
+	}
+}
+int main() {
+	cin>>n>>m;
+	for(int i = 0;i < m;i++) {
+		scanf("%d%d",&a,&b);
+		adj[a].push_back(b);
+		rev[b].push_back(a);
+	}
+	for(int i = 1;i <= n;i++)
+		if(!vis[i])
+			dfs(i);
+	
+	memset(vis,0,sizeof(vis));
+	ct = 1;
+	while(!st.empty()) {
+		int i = st.top();
+		st.pop();
+		if(!vis[i]) {
+			revdfs(i);
+			ct++;
+		}
+	}
+	for(int i = 1;i <= n;i++) {
+		if(mymap[vis[i]] > 1)
+			printf("1 ");
+		else
+			printf("0 ");
+	}
+}
